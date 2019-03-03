23da7f0832efae6009d601c620c5acbde9c6b3cb
Steven <steven.tjg@gmail.com>
2019-03-03 13:24:48 +0000

    commit

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const Campground = require("./models/campground");
+const Comment = require("./models/comment");
+const User = require("./models/user");
+const seedDB = require("./seeds");
 const config = require("./config");
 
 let app = express();
 mongoose.connect(config.mongodb_uri, { useNewUrlParser: true });
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-let Campground = mongoose.model("Campground", campgroundSchema);
+// seedDB();
 
 app.get("/", (req, res) => {
     res.render("landing.ejs");
@@ -26,7 +25,7 @@ app.get("/campgrounds", (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render("index.ejs", {data:campgrounds});
+            res.render("campgrounds/index.ejs", {data:campgrounds});
         }
     });
 });
@@ -46,18 +45,49 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("new.ejs");
+    res.render("campgrounds/new.ejs");
 });
 
 app.get("/campgrounds/:id", (req, res) => {
-    Campground.findById(req.params.id, (err, result) => {
+    Campground.findById(req.params.id).populate("comments").exec((err, result) => {
         if (err) {
             console.log(err);
         } else {
-            res.render("show.ejs", {campground: result});
+            console.log(result);
+            res.render("campgrounds/show.ejs", {campground: result});
         }
     });
 });
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new.ejs", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, (err, comment) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("Server is running.");
---
+++ models/user.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+let User;
+
+module.exports = User;
